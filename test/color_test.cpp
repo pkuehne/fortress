15a152600fe65b6a6851b878bc779769a234de90
Peter Kuehne <peter.kuehne@gmail.com>
2015-06-13 10:24:40 +0100

    Added gtest/gmock support

---
+++ test/color_test.cpp
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+#include <color.h>
+
+TEST (Color, DefaultConstructor)
+{
+    Color color;
+    
+    EXPECT_EQ(0, color.Red());
+    EXPECT_EQ(0, color.Green());
+    EXPECT_EQ(0, color.Blue());
+}
+
+TEST (Color, RGBConstructor)
+{
+    int red     = 10;
+    int green   = 20;
+    int blue    = 30;
+    Color color (red, green, blue);
+    
+    EXPECT_EQ(red, color.Red());
+    EXPECT_EQ(green, color.Green());
+    EXPECT_EQ(blue, color.Blue());
+}
