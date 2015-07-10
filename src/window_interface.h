8667bc4738949ed4bb90bd4dab15ec68d1c325fe
Peter Kuehne <peter.kuehne@gmail.com>
2015-07-10 08:17:59 +0100

    Added the window interface file

---
+++ src/window_interface.h
@@ -0,0 +1,21 @@
+#ifndef WINDOW_INTERFACE_H
+#define WINDOW_INTERFACE_H
+
+class WindowInterface {
+public:
+    virtual ~WindowInterface() {}
+    virtual void initialise (void) = 0;
+    virtual void destroy (void) = 0;
+
+    virtual void keyDown (unsigned char key) = 0;
+    virtual void keyUp (unsigned char key) = 0;
+    virtual bool getKey (unsigned char key) = 0;
+    virtual void mouseDown (int x, int y, int button) = 0;
+    virtual void mouseUp (int x, int y, int button) = 0;
+    virtual void beforeRedraw() = 0;
+    virtual void redraw () = 0;
+    virtual void afterRedraw() = 0;
+    virtual void resize (int width, int height) = 0;
+};
+
+#endif
