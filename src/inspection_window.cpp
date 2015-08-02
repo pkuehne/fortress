9bcd27d1adc37aa9e7033e0a874534e92cfea5f1
Peter Kuehne <peter.kuehne@gmail.com>
2015-08-02 17:11:06 +0100

    InpsectionWindow cleanup

--- src/inspection_window.cpp
+++ src/inspection_window.cpp
@@ -16,29 +16,20 @@ void InspectionWindow::resize() {
     setDimensions (0, 0, width, height);
 }
 
-void InspectionWindow::redraw() {
-
-//    int x = getXOffset();
-//    int y = getYOffset();
-
-    //getEngine()->getGraphics()->calculateWindowOffsetsFromCentre (height, width, x, y);
-    //getEngine()->getGraphics()->clearArea (y, x, height, width);
-    //getEngine()->getGraphics()->drawBorder (y, x, height, width);
-
-    drawString (2, 2, "Description: ");
-
-    EntityId* l_entity = static_cast<EntityId*>(getArgs());
-    if (!l_entity) return;
-
-    DescriptionComponent* l_description = getEngine()->getEntities()->getDescriptions()->get(*l_entity);
-    if (!l_description) return;
-
-    drawString (2, 15, l_description->title.c_str());
-    drawString (4,  2, l_description->text.c_str());
+void InspectionWindow::redraw()
+{
+    if (!m_description) return;
+    drawString (3,  2, m_description->text.c_str());
 }
 
 void InspectionWindow::gainFocus() {
-    setTitle ("What?");
+    EntityId* l_entity = static_cast<EntityId*>(getArgs());
+    if (!l_entity) return;
+
+    m_description = getEngine()->getEntities()->getDescriptions()->get(*l_entity);
+    if (!m_description) return;
+
+    setTitle (m_description->title);
 }
 
 void InspectionWindow::loseFocus() {
--- src/inspection_window.h
+++ src/inspection_window.h
@@ -2,15 +2,18 @@
 #define INSPECTION_WINDOW_H
 
 #include "window.h"
+#include "description_component.h"
 
 class InspectionWindow : public Window {
 public:
     void keyDown (unsigned char key);
     void redraw();
     void resize();
-    
+
     void gainFocus();
     void loseFocus();
+private:
+    DescriptionComponent*   m_description;
 };
 
 #endif
