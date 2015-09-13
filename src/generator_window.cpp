3553e3313e9a4dc80edcf857966b167f1d6b31dd
Peter Kuehne <peter.kuehne@gmail.com>
2015-09-13 19:35:46 +0100

    Added Window for generating Worlds

---
+++ src/generator_window.cpp
@@ -0,0 +1,1 @@
+#include "generator_window.h"
---
+++ src/generator_window.h
@@ -0,0 +1,11 @@
+#ifndef GENERATOR_WINDOW
+#define GENERATOR_WINDOW
+
+#include "window.h"
+
+class GeneratorWindow : public Window {
+public:
+
+private:
+
+};
