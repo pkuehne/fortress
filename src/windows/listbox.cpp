225651dcc2ca84c77d3b9c7fba1daf8572854445
Peter Kuehne <pkuehne@bloomberg.net>
2017-10-30 07:34:31 +0000

    ListBox scrolling

--- src/windows/listbox.cpp
+++ src/windows/listbox.cpp
@@ -8,11 +8,11 @@ void ListBox::render()
         if (ii >= items.size()) {
             break;
         }
-        if (selectedItem == ii) {
+        if (selectedItem-m_topOffset == ii) {
             this->drawString(0, ii, ">", COLOR(RED));
         }
 
-        this->drawString(1, ii, items[ii].c_str());
+        this->drawString(1, ii, items[ii+m_topOffset].c_str());
     }
 }
 
@@ -20,9 +20,14 @@ void ListBox::keyPress(unsigned char key)
 {
     if (key == '+') {
         selectedItem = (selectedItem == items.size()-1) ? items.size()-1: selectedItem+1;
+        if (selectedItem >= this->height) {
+            m_topOffset = selectedItem - this->height + 1;
+        }
     }
     if (key == '-') {
         selectedItem = (selectedItem == 0) ? 0 : selectedItem-1;
-        
+        if (selectedItem < m_topOffset) {
+            m_topOffset = 0;
+        }
     }
 }
